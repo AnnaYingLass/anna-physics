7786f0db0cbecf1567964369ed13aa84d6351753
Anna Ying <annayingwork@gmail.com>
2021-08-17 11:47:15 +0800

    deleted landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,5 @@
 class PagesController < ApplicationController
-  def landing
-  end
-
   def home
     @students = ['Jasmine', 'Jasmine\'s friend']
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#landing', as: :landing
-  get 'home', to: 'pages#home', as: :home
+
+  root to: 'pages#home', as: :home
   get 'motor_theory', to: 'pages#motor_theory', as: :motor_theory
   get 'generator_theory', to: 'pages#generator_theory', as: :generator_theory
 
