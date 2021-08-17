4b3583cbb2e548ea86981e5b75ceccc00a698ff7
Anna Ying <annayingwork@gmail.com>
2021-08-17 13:26:05 +0800

    added B-field

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,10 @@ class PagesController < ApplicationController
   def home
     @students = ['Jasmine', 'Jasmine\'s friend']
   end
+
+  def e_field
+  end
+
+  def b_field
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: 'pages#home', as: :home
   get 'motor_theory', to: 'pages#motor_theory', as: :motor_theory
   get 'generator_theory', to: 'pages#generator_theory', as: :generator_theory
+  get 'b_field', to: 'pages#b_field', as: :b_field
+  get 'e_field', to: 'pages#e_field', as: :e_field
 
   get 'ask', to: 'questions#ask', as: :ask
   get 'answer', to: 'questions#answer', as: :answer
